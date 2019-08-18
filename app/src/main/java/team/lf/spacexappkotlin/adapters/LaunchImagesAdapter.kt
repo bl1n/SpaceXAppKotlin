e33846370ee8c74e297eaff64d79feb5b8871bf7
bl1n <Qwe!35135>
2019-08-18 18:27:14 +0300

    layoutManager + landscape

--- app/src/main/java/team/lf/spacexappkotlin/adapters/LaunchImagesAdapter.kt
+++ app/src/main/java/team/lf/spacexappkotlin/adapters/LaunchImagesAdapter.kt
@@ -4,6 +4,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.li_launch_image.view.*
 import team.lf.spacexappkotlin.R
 import team.lf.spacexappkotlin.rest.models.Launch
@@ -18,7 +21,13 @@ class LaunchImagesAdapter: BaseAdapter<LaunchImagesAdapter.LaunchImageViewHolder
         override fun bind(item: Any) {
             let {
                 item as String
-                Glide.with(view.context).load(item).into(view.li_launch_image)
+                Picasso.get().load(item).resize(450,450).placeholder(R.drawable.ic_spacex_vector_logo).into(view.li_launch_image)
+//                Glide.with(view.context)
+//                    .load(item)
+//                    .placeHolder()
+//                    .into(view.li_launch_image)
+
+
             }
 
         }
